af28d2849d11a3d15f0296b58f34663d5a100da1
adrianogiannigh <adrianogianni14@gmail.com>
2024-11-03 15:05:20 +0100

    Agregado entidad usuario

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,31 @@
+﻿namespace API.Entities
+{
+    /// <summary>
+    /// Define un usuario.
+    /// </summary>
+    public class AppUser
+    {
+        #region Properties 
+
+        /// <summary>
+        /// Identidad del usuario. 
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Nombre del usuario. 
+        /// </summary>
+        public required string UserName { get; set; }
+
+        #endregion
+
+        #region Fields
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+
+    }
+}
