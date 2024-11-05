a793acd972aa872ce22bdf4c2099f14c5c202388
adrianogiannigh <adrianogianni14@gmail.com>
2024-11-05 07:04:24 +0100

    Added DataBase

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    /// <summary>
+    /// Hace referencia al contexto de los datos para su tratamiento en BD. 
+    /// </summary>
+    /// <param name="options"></param>
+    public class DataContext(DbContextOptions options) : DbContext(options)
+    {
+        /// <summary>
+        /// Columna de Usuarios en BD.
+        /// </summary>
+        public DbSet<AppUser> Users { get; set; }
+
+    }
+}
