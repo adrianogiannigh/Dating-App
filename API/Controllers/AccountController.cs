5ed0ecbab4749dac0b934c42d8b47bf6ecd7bc60
adrianogiannigh <adrianogianni14@gmail.com>
2024-11-10 16:09:13 +0100

    Added Login Freature, Added launch.json for debugging

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -13,6 +13,11 @@ namespace API.Controllers
     /// </summary>
     public class AccountController(DataContext context) : BaseApiController
     {
+        /// <summary>
+        /// Endpoint para registar un usuario.
+        /// </summary>
+        /// <param name="registerDto"></param>
+        /// <returns></returns>
         [HttpPost("register")] // account/register
         public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
         {
@@ -30,6 +35,27 @@ namespace API.Controllers
             await context.SaveChangesAsync();
 
             return user; 
+        }
+        /// <summary>
+        /// Endpoint para logear un usuario. 
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await context.Users.FirstOrDefaultAsync(p =>
+            p.UserName == loginDto.UserName.ToLower()
+            );
+
+            if (user == null) return Unauthorized("Invalid User Name");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Passoword");
+            }
+            return user;
         }
 
         private async Task<bool> UserExist(string username)
