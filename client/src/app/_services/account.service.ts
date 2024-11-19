1409e4fdddc91b2aa6aaca4edbbc705de2491cf1
adrianogiannigh <adrianogianni14@gmail.com>
2024-11-19 14:29:59 +0100

    Created Home Component

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,13 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
-import { User } from '../_models/User';
+import { User } from '../_models/user';
 import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-
+  
   private http = inject(HttpClient);
   
   baseUrl = "https://localhost:5001/api/";
