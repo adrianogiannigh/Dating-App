46be2fdf74ec35bda62ce90ebcec2abb62bcc5ed
adrianogiannigh <adrianogianni14@gmail.com>
2024-11-19 14:04:26 +0100

    Added Persistance in LogIn

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -20,18 +21,31 @@ export class AppComponent implements OnInit{
   //Added injected Client in the Component   
   //constructor(private httpClient: HttpClient) {}   ////constructor way 
   http = inject(HttpClient); 
+  private accountService = inject(AccountService);
   users : any;  
 
   //--------------------------------------------
 
   ngOnInit(): void {
-    //getting the endpoint  
+    this.getUsers();
+    this.setCurrentUser();    
+  }
+
+  setCurrentUser()
+  {
+    const userString = localStorage.getItem('user');
+    if(!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+  }
+
+  getUsers() {
+      //getting the endpoint  
     this.http.get('https://localhost:5001/api/users').subscribe({
       next: response => this.users = response,
       error: error => console.log(error),
       complete: () => console.log('Request has completed')
     }) // Return an observable of the response body as a JavaScript object
-
   }
 }
  
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,23 +1,22 @@
 import { Component, inject , } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,BsDropdownModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
-private accountService = inject(AccountService);
-loggedIn  = false;
+accountService = inject(AccountService);
 model : any = {};
   login() {
     this.accountService.login(this.model).subscribe({
       next: response => {
-        console.log(response);
-        this.loggedIn = true;
+        console.log(response);        
       },
       error: error => {
         console.log(error);
@@ -26,6 +25,6 @@ model : any = {};
   }
 
   logout() {
-    this.loggedIn = false;
+    this.accountService.logout();
   }
 }
